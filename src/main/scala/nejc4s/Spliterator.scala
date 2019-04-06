08b1862d5c557957986ca0c9a1246882225e91b0
Nicholas Yip <nicholasyip@obceit.cc>
2019-04-07 03:56:22 +0900

    Add type alias for `Spliterator.OfPrimitive`

--- src/main/scala/nejc4s/Spliterator.scala
+++ src/main/scala/nejc4s/Spliterator.scala
@@ -6,4 +6,5 @@ object Spliterator {
   type OfDouble = util.Spliterator.OfDouble
   type OfInt = util.Spliterator.OfInt
   type OfLong = util.Spliterator.OfLong
+  type OfPrimitive[A, C, S <: OfPrimitive[A, C, S]] = util.Spliterator.OfPrimitive[A, C, S]
 }
