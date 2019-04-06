5156c82426f9f69e8729f8e0fae428bb8603cba5
Nicholas Yip <nicholasyip@obceit.cc>
2019-04-06 18:14:02 +0900

    Add `JavaCollection.UnsafeProxy`

--- src/main/scala/nejc4s/JavaCollection.scala
+++ src/main/scala/nejc4s/JavaCollection.scala
@@ -1,5 +1,7 @@
 package nejc4s
 
+import java.util
+
 object JavaCollection {
   trait Base[A] extends JavaCollection[A] with JavaIterable.Base[A] {
     override def size: NonNegativeInt
@@ -12,5 +14,21 @@ object JavaCollection {
     //override def spliterator: JavaSpliterator[A] = super.spliterator
     //override def stream: JavaStream[A] = super.stream
     //override def parallelStream: JavaStream[A] = super.parallelStream
+  }
+
+  trait UnsafeProxy[A] extends util.AbstractCollection[A] with Base[A] with JavaIterable.UnsafeProxy[A] {
+    protected
+    def delegate: JavaCollection[A]
+
+    override def size: NonNegativeInt = NonNegativeInt.unsafeFromInt(delegate.size)
+    override def isEmpty: Boolean = delegate.isEmpty
+    override def contains(o: Any): Boolean = delegate.contains(o)
+    override def toArray: Array[AnyRef] = delegate.toArray()
+    //override def toArray[T](a: Array[T]): Array[T]
+    override def containsAll(c: JavaCollection[_]): Boolean = delegate.containsAll(c)
+
+    //override def spliterator: JavaSpliterator[A] = delegate.spliterator
+    override def stream: JavaStream[A] = delegate.stream
+    override def parallelStream: JavaStream[A] = delegate.parallelStream
   }
 }
--- src/main/scala/nejc4s/JavaIterable.scala
+++ src/main/scala/nejc4s/JavaIterable.scala
@@ -1,10 +1,22 @@
 package nejc4s
 
+import java.util.function.Consumer
+
 object JavaIterable {
   trait Base[A] extends JavaIterable[A] {
     //override def iterator: JavaIterator[A]
 
     //override def forEach(action: Consumer[_ >: A]): Unit = super.forEach(action)
     //override def spliterator: JavaSpliterator[A] = super.spliterator
+  }
+
+  trait UnsafeProxy[A] extends Base[A] {
+    protected
+    def delegate: JavaIterable[A]
+
+    override def iterator: JavaIterator[A] = delegate.iterator
+
+    override def forEach(action: Consumer[_ >: A]): Unit = delegate.forEach(action)
+    override def spliterator: JavaSpliterator[A] = delegate.spliterator
   }
 }
