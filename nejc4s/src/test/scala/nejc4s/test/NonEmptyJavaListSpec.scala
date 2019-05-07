a0c07cec1b470a85d7d2024e9f4e206f78391527
Nicholas Yip <nicholasyip@obceit.cc>
2019-05-07 14:51:04 +0900

    Add test case for `NonEmptyJavaList.unapplySeq`

--- nejc4s/src/test/scala/nejc4s/test/NonEmptyJavaListSpec.scala
+++ nejc4s/src/test/scala/nejc4s/test/NonEmptyJavaListSpec.scala
@@ -11,5 +11,15 @@ class NonEmptyJavaListSpec extends FreeSpec {
     "`apply` should work" in {
       NonEmptyJavaList("a", "b", "c")
     }
+
+    "`unapplySeq` should work" in {
+      import scala.collection.JavaConverters._
+
+      NonEmptyJavaList.unapplySeq((0 until 10).asJava)
+
+      (0 until 3).asJava match {
+        case NonEmptyJavaList(0, 1, 2) =>
+      }
+    }
   }
 }
