92872b6f317c491064216a09e71bbb3b883bcf9f
Nicholas Yip <nicholasyip@obceit.cc>
2019-05-16 13:47:09 +0900

    Test all synthesized methods

--- nejc4s/src/test/scala/nejc4s/test/NonEmptyJavaListSpec.scala
+++ nejc4s/src/test/scala/nejc4s/test/NonEmptyJavaListSpec.scala
@@ -1,6 +1,9 @@
 package nejc4s
 package test
 
+import java.lang.reflect.InvocationTargetException
+
+import nejc4s.base.{JavaList, Spliterator}
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatestplus.junit.JUnitRunner
@@ -8,8 +11,26 @@ import org.scalatestplus.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class NonEmptyJavaListSpec extends FreeSpec {
   "NonEmptyJavaList" - {
-    "`apply` should work" in {
-      NonEmptyJavaList("a", "b", "c")
+    "`spliterator` should work" in {
+      val l = NonEmptyJavaList("a", "b", "c")
+
+      l.spliterator
+      (l: NonEmptyJavaCollection[String]).spliterator
+      (l: JavaList[String]).spliterator
+      (l: JavaList.Refined[String]).spliterator
+
+      l
+        .getClass
+        .getMethods
+        .filter(m => classOf[Spliterator[_]].isAssignableFrom(m.getReturnType))
+        .foreach { m =>
+          try {
+            m.invoke(l)
+          } catch {
+            case e: InvocationTargetException =>
+              throw e.getTargetException
+          }
+        }
     }
 
     "`unapplySeq` should work" in {
